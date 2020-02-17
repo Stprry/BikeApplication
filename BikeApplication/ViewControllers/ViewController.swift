2ba4a91a0131de3954a6e28fd60aaee495132eee
Stprry <stprry96@gmail.com>
2020-02-17 21:49:05 +0000

    Installed Firebase

--- BikeApplication/ViewControllers/ViewController.swift
+++ BikeApplication/ViewControllers/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var RegisterBtn: UIButton!
+    
+    @IBOutlet weak var LoginBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        frontEndSetUp()
     }
-
-
+    
+    func frontEndSetUp(){
+        Utilities.styleFilledButton(RegisterBtn)
+        Utilities.styleHollowButton(LoginBtn)
+    }
+    
+    
+    @IBAction func LoginBtnTap(_ sender: Any) {
+        
+    }
+    
+    @IBAction func RegisterBtnTap(_ sender: Any) {
+        
+    }
 }
 
