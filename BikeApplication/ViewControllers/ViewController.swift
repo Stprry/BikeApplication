56e957c03a0f91e335f595239287b698301a9eb0
Stprry <stprry96@gmail.com>
2020-02-18 00:43:34 +0000

    Video Background working, log in and sign up working, Need to implement back button on login page and register page to return to home page, then should progress to main page and other navies

--- BikeApplication/ViewControllers/ViewController.swift
+++ BikeApplication/ViewControllers/ViewController.swift
@@ -11,8 +11,9 @@ import AVKit
 
 class ViewController: UIViewController {
 
-    var videoPlay:AVPlayer?
-    var videoPlayLayer:AVPlayerLayer?
+    var videoPlayer:AVPlayer?
+       
+    var videoPlayerLayer:AVPlayerLayer?
     
     @IBOutlet weak var RegisterBtn: UIButton!
     
@@ -25,36 +26,44 @@ class ViewController: UIViewController {
         frontEndSetUp()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        
+        // Set up video in the background
+        setUpVideo()
+    }
+    
     func frontEndSetUp(){
         Utilities.styleFilledButton(RegisterBtn)
         Utilities.styleHollowButton(LoginBtn)
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        // set up background video
-        setUpBg()
-    }
-    func setUpBg(){
-       let bundlePath = Bundle.main.path(forResource: "bike", ofType: "mp4")
-        
-        guard bundlePath != nil else {
-            return
-        }
-        let url = URL(fileURLWithPath: bundlePath!)
-        
+     func setUpVideo() {
+          
+          // Get the path to the resource in the bundle
+          let bundlePath = Bundle.main.path(forResource: "bike", ofType: "mp4")
+          
+          guard bundlePath != nil else {
+              return
+          }
+          
+          let url = URL(fileURLWithPath: bundlePath!)
+          
+          let item = AVPlayerItem(url: url)
+          
+          videoPlayer = AVPlayer(playerItem: item)
+          
+          // Create the layer
+          videoPlayerLayer = AVPlayerLayer(player: videoPlayer!)
+          
+          // Adjust the size and frame
+          videoPlayerLayer?.frame = CGRect(x: -self.view.frame.size.width*1.5, y: 0, width: self.view.frame.size.width*4, height: self.view.frame.size.height)
+          
+          view.layer.insertSublayer(videoPlayerLayer!, at: 0)
+          
+          // Add it to the view and play it
+          videoPlayer?.playImmediately(atRate: 0.3)
+      }
 
-        let avItem = AVPlayerItem(url:url)
-        
-         videoPlay = AVPlayer(playerItem: avItem)
-        
-        // Adjust the size and frame
-        videoPlayLayer?.frame = CGRect(x: -self.view.frame.size.width*1.5, y: 0, width: self.view.frame.size.width*4, height: self.view.frame.size.height)
-        
-        view.layer.insertSublayer(videoPlayLayer!, at: 0)
-        
-        // Add it to the view and play it
-        videoPlayLayer?.player?.playImmediately(atRate: 0.3)
-    }
     
     @IBAction func LoginBtnTap(_ sender: Any) {
         
