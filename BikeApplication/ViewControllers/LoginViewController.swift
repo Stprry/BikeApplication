43cad5d4c6240812b0bf9dd8a40696c956534a3d
Stprry <stprry96@gmail.com>
2020-02-19 17:09:50 +0000

    Log in button broke implemented new one

--- BikeApplication/ViewControllers/LoginViewController.swift
+++ BikeApplication/ViewControllers/LoginViewController.swift
@@ -21,6 +21,8 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var ErrorMsg: UILabel!
     
     
+    @IBOutlet weak var LoginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -32,29 +34,31 @@ class LoginViewController: UIViewController {
         ErrorMsg.alpha = 0
         Utilities.styleTextField(EmailFeild)
         Utilities.styleTextField(PasswordFeild)
-        Utilities.styleHollowButton(LoginBtn)
+        Utilities.styleFilledButton(LoginButton)
     }
 
-    @IBAction func LoginTap(_ sender: Any) {
+    @IBAction func LoginButtonTapped(_ sender: Any) {
+        
         // validate feilds still to impliment
-        
-        
-        //if all feilds valid do below
-        let email = EmailFeild.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        let password = PasswordFeild.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        
-        // sign in user
-        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
-            if error != nil{
-                // sign in error
-                self.ErrorMsg.text = error!.localizedDescription
-                self.ErrorMsg.alpha = 1
-            }else{
-                // go to home screen
-                self.transitionHome()
+            
+            
+            //if all feilds valid do below
+            let email = EmailFeild.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = PasswordFeild.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            // sign in user
+            Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+                if error != nil{
+                    // sign in error
+                    self.ErrorMsg.text = error!.localizedDescription
+                    self.ErrorMsg.alpha = 1
+                }else{
+                    // go to home screen
+                    self.transitionHome()
+                }
             }
-        }
     }
+    
     func transitionHome(){
         let homeViewController =
         storyboard?.instantiateViewController(identifier: Constants.Storyboards.homeViewContrl) as? HomeViewController
