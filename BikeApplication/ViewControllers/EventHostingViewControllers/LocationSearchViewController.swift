7efd326a6febcfda1f7f115fcf6e460d93e681d5
Stprry <stprry96@gmail.com>
2020-03-16 18:03:20 +0000

    Introduced reverse geolocation to get dress of a pin location, will be used to set a ride location meet up point for events

--- BikeApplication/ViewControllers/EventHostingViewControllers/LocationSearchViewController.swift
+++ BikeApplication/ViewControllers/EventHostingViewControllers/LocationSearchViewController.swift
@@ -11,12 +11,13 @@ import MapKit
 import CoreLocation
 
 class LocationSearchViewController: UIViewController {
-    
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var adressLabel: UILabel!
     
     let locationManager = CLLocationManager()
     let regionInMeters: Double = 10000
-    
+    var previousLocation: CLLocation?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         checkLocationServices()
@@ -45,15 +46,10 @@ class LocationSearchViewController: UIViewController {
             // Show alert letting the user know they have to turn this on.
         }
     }
-    
-    
-    func checkLocationAuthorization() {
+        func checkLocationAuthorization() {
         switch CLLocationManager.authorizationStatus() {
         case .authorizedWhenInUse:
-            mapView.showsUserLocation = true
-            centerViewOnUserLocation()
-            locationManager.startUpdatingLocation()
-            break
+            startTackingUserLocation()
         case .denied:
             // Show alert instructing them how to turn on permissions
             break
@@ -66,19 +62,64 @@ class LocationSearchViewController: UIViewController {
             break
         }
     }
+    
+    func startTackingUserLocation() {
+        mapView.showsUserLocation = true
+        centerViewOnUserLocation()
+        locationManager.startUpdatingLocation()
+        previousLocation = getCenterLocation(for: mapView)
+    }
+    
+    func getCenterLocation(for mapView: MKMapView) -> CLLocation {
+           let latitude = mapView.centerCoordinate.latitude
+           let longitude = mapView.centerCoordinate.longitude
+           
+           return CLLocation(latitude: latitude, longitude: longitude)
+       }
 }
 
 
 extension LocationSearchViewController: CLLocationManagerDelegate {
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        let region = MKCoordinateRegion.init(center: location.coordinate, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
-        mapView.setRegion(region, animated: true)
-    }
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        guard let location = locations.last else { return }
+//        let region = MKCoordinateRegion.init(center: location.coordinate, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+//        mapView.setRegion(region, animated: true)
+//    }
     
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         checkLocationAuthorization()
+    }
+}
+
+extension LocationSearchViewController: MKMapViewDelegate{
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+    let center = getCenterLocation(for: mapView)
+    let geoCoder = CLGeocoder()
+        
+        guard let previousLocation = self.previousLocation else { return }
+        guard center.distance(from: previousLocation) > 50 else { return }/// call previous location if greater than t0 meters from last
+        self.previousLocation = center
+        
+        geoCoder.reverseGeocodeLocation(center) { [weak self] (placemarks, error) in
+        
+            guard let self = self else { return }
+            if let _ = error {
+                //TODO: Show alert informing the user
+                return
+            }
+            guard let placemark = placemarks?.first else {
+                //TODO: Show alert informing the user
+                return
+            }
+             let roadNum = placemark.subThoroughfare ?? ""
+             let roadName = placemark.thoroughfare ?? ""
+             let postcode = placemark.postalCode ?? ""
+            
+            DispatchQueue.main.async {
+                self.adressLabel.text = "\(roadNum) \(roadName) \(postcode)"
+            }
+        }
     }
 }
